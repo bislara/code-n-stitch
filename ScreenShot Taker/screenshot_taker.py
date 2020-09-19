3b3624715588751a8351305721421b23fe54e828
bislara <biswajeetsahoo54@gmail.com>
2020-09-19 17:34:33 +0530

    added the executable file for the game

---
+++ ScreenShot Taker/screenshot_taker.py
@@ -0,0 +1,21 @@
+# Python program to take 
+# screenshots 
+
+
+import numpy as np 
+import cv2 
+import pyautogui 
+
+
+# take screenshot using pyautogui 
+image = pyautogui.screenshot() 
+
+# since the pyautogui takes as a 
+# PIL(pillow) and in RGB we need to 
+# convert it to numpy array and BGR 
+# so we can write it to the disk 
+image = cv2.cvtColor(np.array(image), 
+					cv2.COLOR_RGB2BGR) 
+
+# writing it to the disk using opencv 
+cv2.imwrite("image1.png", image) 
