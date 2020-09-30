d476ce6dc41629c79c379d12a360f932b8f33c98
Asit Kumar <49069830+CapAsdour@users.noreply.github.com>
2020-10-01 02:12:13 +0530

    Update text_from_image.py

--- Text_From_Image/text_from_image.py
+++ Text_From_Image/text_from_image.py
@@ -7,5 +7,5 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract' #path location of library
 TESSDATA_PREFIX = 'C:/Program Files (x86)/Tesseract-OCR'
-output = pytesseract.image_to_string(PIL.Image.open('Image1.PNG').convert("RGB"), lang='eng')
+output = pytesseract.image_to_string(PIL.Image.open('Image1.PNG').convert("RGB"), lang='eng') #set the name of image you want to read
 print output
