233a0908fd2a452ab5ffaca260995c305b97b2b2
UbadahJ <ubadahjafry@gmail.com>
2020-10-01 12:06:51 +0500

    Reformatted code

--- Python/PhoneNumberExtractor/src/phonenumber.py
+++ Python/PhoneNumberExtractor/src/phonenumber.py
@@ -1,13 +1,11 @@
 inp = input("please enter the text")
 
-print("The original string : " + inp) 
-  
+print("The original string : " + inp)
 
-res = [int(i) for i in inp.split() if i.isdigit()] 
+
+res = [int(i) for i in inp.split() if i.isdigit()]
 # this basically makes a list of all the numbers
 
 for i in res:
-    if len(str(i))==10:
+    if len(str(i)) == 10:
         print("the phone number in the text is:" + str(i))
-
-
