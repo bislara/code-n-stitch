95cea84adfab480d6aac133724d6fd89f73b3c08
Pranav  Natekar <pranavnat24@gmail.com>
2020-10-01 15:39:38 +0530

    Added comments and screenshots

--- Paint Application/main.py
+++ Paint Application/main.py
@@ -8,41 +8,65 @@ class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("PyPaint")
+        # setting title
+        self.setWindowTitle("Paint with PyQt5")
+
+        # setting geometry to main window
         self.setGeometry(100, 100, 800, 600)
 
+        # creating image object
         self.image = QImage(self.size(), QImage.Format_RGB32)
 
+        # making image color to white
         self.image.fill(Qt.white)
 
+        # variables
+        # drawing flag
         self.drawing = False
+        # default brush size
         self.brushSize = 2
+        # default color
         self.brushColor = Qt.black
 
+        # QPoint object to tract the point
         self.lastPoint = QPoint()
 
+        # creating menu bar
         mainMenu = self.menuBar()
 
+        # creating file menu for save and clear action
         fileMenu = mainMenu.addMenu("File")
 
+        # adding brush size to main menu
         b_size = mainMenu.addMenu("Brush Size")
 
+        # adding brush color to ain menu
         b_color = mainMenu.addMenu("Brush Color")
 
+        # creating save action
         saveAction = QAction("Save", self)
 
+        # adding save to the file menu
         fileMenu.addAction(saveAction)
+        # adding action to the save
         saveAction.triggered.connect(self.save)
 
+        # creating clear action
         clearAction = QAction("Clear", self)
-
+        # adding clear to the file menu
         fileMenu.addAction(clearAction)
+        # adding action to the clear
         clearAction.triggered.connect(self.clear)
 
+        # creating options for brush sizes
+        # creating action for selecting pixel of 4px
         pix_4 = QAction("4px", self)
+        # adding this action to the brush size
         b_size.addAction(pix_4)
+        # adding method to this
         pix_4.triggered.connect(self.Pixel_4)
 
+        # similarly repeating above steps for different sizes
         pix_7 = QAction("7px", self)
         b_size.addAction(pix_7)
         pix_7.triggered.connect(self.Pixel_7)
@@ -55,10 +79,15 @@ class Window(QMainWindow):
         b_size.addAction(pix_12)
         pix_12.triggered.connect(self.Pixel_12)
 
+        # creating options for brush color
+        # creating action for black color
         black = QAction("Black", self)
+        # adding this action to the brush colors
         b_color.addAction(black)
+        # adding methods to the black
         black.triggered.connect(self.blackColor)
 
+        # similarly repeating above steps for different color
         white = QAction("White", self)
         b_color.addAction(white)
         white.triggered.connect(self.whiteColor)
@@ -75,35 +104,55 @@ class Window(QMainWindow):
         b_color.addAction(red)
         red.triggered.connect(self.redColor)
 
+    # method for checking mouse cicks
     def mousePressEvent(self, event):
 
+        # if left mouse button is pressed
         if event.button() == Qt.LeftButton:
+            # make drawing flag true
             self.drawing = True
+            # make last point to the point of cursor
             self.lastPoint = event.pos()
+
+        # method for tracking mouse activity
 
     def mouseMoveEvent(self, event):
 
+        # checking if left button is pressed and drawing flag is true
         if (event.buttons() & Qt.LeftButton) & self.drawing:
+            # creating painter object
             painter = QPainter(self.image)
 
+            # set the pen of the painter
             painter.setPen(QPen(self.brushColor, self.brushSize,
                                 Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
 
+            # draw line from the last point of cursor to the current point
+            # this will draw only one step
             painter.drawLine(self.lastPoint, event.pos())
 
+            # change the last point
             self.lastPoint = event.pos()
+            # update
             self.update()
+
+        # method for mouse left button release
 
     def mouseReleaseEvent(self, event):
 
         if event.button() == Qt.LeftButton:
+            # make drawing flag false
             self.drawing = False
 
+    # paint event
     def paintEvent(self, event):
+        # create a canvas
         canvasPainter = QPainter(self)
 
+        # draw rectangle on the canvas
         canvasPainter.drawImage(self.rect(), self.image, self.image.rect())
 
+    # method for saving canvas
     def save(self):
         filePath, _ = QFileDialog.getSaveFileName(self, "Save Image", "",
                                                   "PNG(*.png);;JPEG(*.jpg *.jpeg);;All Files(*.*) ")
@@ -112,10 +161,14 @@ class Window(QMainWindow):
             return
         self.image.save(filePath)
 
+    # method for clearing every thing on canvas
     def clear(self):
+        # make the whole canvas white
         self.image.fill(Qt.white)
+        # update
         self.update()
 
+    # methods for changing pixel sizes
     def Pixel_4(self):
         self.brushSize = 4
 
@@ -128,6 +181,7 @@ class Window(QMainWindow):
     def Pixel_12(self):
         self.brushSize = 12
 
+    # methods for changing brush color
     def blackColor(self):
         self.brushColor = Qt.black
 
@@ -143,11 +197,16 @@ class Window(QMainWindow):
     def redColor(self):
         self.brushColor = Qt.red
 
+    # create pyqt5 app
+
 
 App = QApplication(sys.argv)
 
+# create the instance of our Window
 window = Window()
 
+# showing the wwindow
 window.show()
 
+# start the app
 sys.exit(App.exec())
