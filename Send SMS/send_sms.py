8ec4abd5baa7741077950a2953f9d239f8687665
gshekler <gshekler@proofpoint.com>
2020-10-03 21:15:18 +0300

    add phone number validation

--- Send SMS/send_sms.py
+++ Send SMS/send_sms.py
@@ -1,4 +1,5 @@
 from twilio.rest import Client
+import phonenumbers
 
 account_sid = 'your_acount_sid'
 auth_token = 'your_auth_token'
@@ -15,10 +16,25 @@ class TwilioClient:
     def send_messages(self, message, phone_number):
         self.client.messages.create(from_=self.sender_number, body=message, to=phone_number)
 
+    @staticmethod
+    def is_valid_number(number):
+        if not isinstance(number, str):
+            number = str(number)
+        try:
+            parsed = phonenumbers.parse(number)
+            return phonenumbers.is_valid_number(parsed)
+        except phonenumbers.phonenumberutil.NumberParseException:
+            return False
+
 
 if __name__ == "__main__":
     client = TwilioClient(account_sid=account_sid, auth_token=auth_token, sender_number=sender_number)
 
     message = str(input("Enter SMS message: "))
     phone_number = input("Enter destination phone number for your SMS (with a '+' and a country code): ")
+    while not client.is_valid_number(phone_number):
+        phone_number = input(
+            "Invalid phone number\nPlease enter destination phone number for your SMS (with a '+' and a country code): "
+        )
+
     client.send_messages(message=message, phone_number=phone_number)
