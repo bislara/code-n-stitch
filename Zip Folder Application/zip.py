6100d664a9063ef7c14f928402ba3c0080bb907f
Double 77 <danielread55@hotmail.co.uk>
2020-10-07 10:27:05 +0100

    Update zip.py

--- Zip Folder Application/zip.py
+++ Zip Folder Application/zip.py
@@ -4,21 +4,22 @@ import tkinter as Tk
 from tkinter import filedialog
 
 
-app = Tk()
+app = Tk() 
 
+# Defines a function for the user to be able to select the folder they wish to Zip
 def browse():
-    current_path = os.path.abspath(os.getcwd())
-    currentPath.config(text=current_path)
-    pathname = filedialog.askdirectory()
-    name = pathname.split('/')
-    filename = name[-1] 
-    pathlabel.config(text=pathname)
+    current_path = os.path.abspath(os.getcwd()) # Gets the current path of the application
+    currentPath.config(text=current_path) # Appends the current path to the currentPath label
+    pathname = filedialog.askdirectory() 
+    name = pathname.split('/') # Splits the pathname on "/" 
+    filename = name[-1] # Gets the name of the folder selected from the pathname (The last section of the path)
+    pathlabel.config(text=pathname) # Appends the pathname to the pathlabel label
     shutil.make_archive(filename, 'zip', pathname)
 
-browsebutton = Button(app, text="Find & Zip Folder", command=browse, font=("bold", 14),)
+browsebutton = Button(app, text="Find & Zip Folder", command=browse, font=("bold", 14),) # Sets up the button for finding the folder to Zip
 browsebutton.grid(row=0,column=2, pady=20, padx=40)
 
-# Path of the selected File
+# Sets up the labels & selection boxes
 boxLabel = Label(app, text="Path Of The Folder Selected")
 boxLabel.grid(row=1,column=2, padx=40)
 pathlabel = Label(app)
@@ -29,7 +30,7 @@ boxLabel2.grid(row=3,column=2, padx=40)
 currentPath = Label(app)
 currentPath.grid(row=4,column=2, padx=40)
 
-
+# Sets up the application title, background colour and window size
 app.title("Zip FIle")
 app.geometry("250x180")
 app.configure(bg='#708090')
