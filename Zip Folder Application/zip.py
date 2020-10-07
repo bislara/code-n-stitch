cf494b2d1ca6af1613c8f6c46f14450bcb55bb60
Double 77 <danielread55@hotmail.co.uk>
2020-10-07 12:53:22 +0100

    Updated zip.py - Additional Features
    - Added some extra features such as allowing the user to change the zip name, instead of using the folder name. 
    - Cleaned up the GUI to use columns more effectively
    - Removed the window size to allow it to resize according to the file path length

--- Zip Folder Application/zip.py
+++ Zip Folder Application/zip.py
@@ -1,38 +1,63 @@
-import shutil
-import os
-import tkinter as Tk
+import shutil, os
+from tkinter import *
 from tkinter import filedialog
 
+# Default Zipped Folder Name (If the user selects no name, use this)
+zipname = "zipped_folder"
 
-app = Tk() 
-
-# Defines a function for the user to be able to select the folder they wish to Zip
+# Define function to allow the user to browse to find a folder to zip
 def browse():
-    current_path = os.path.abspath(os.getcwd()) # Gets the current path of the application
-    currentPath.config(text=current_path) # Appends the current path to the currentPath label
-    pathname = filedialog.askdirectory() 
-    name = pathname.split('/') # Splits the pathname on "/" 
-    filename = name[-1] # Gets the name of the folder selected from the pathname (The last section of the path)
-    pathlabel.config(text=pathname) # Appends the pathname to the pathlabel label
-    shutil.make_archive(filename, 'zip', pathname)
+    global pathname 
+    current_path = os.path.abspath(os.getcwd())
+    currentPath.config(text=current_path)
+    pathname = filedialog.askdirectory()
+    pathlabel.config(text=pathname)
+    zipButton["state"] = "normal"
 
-browsebutton = Button(app, text="Find & Zip Folder", command=browse, font=("bold", 14),) # Sets up the button for finding the folder to Zip
-browsebutton.grid(row=0,column=2, pady=20, padx=40)
+# Define function to zip the selected folder
+def zipFolder():
+    global zipname
+    if zip_name_area.get():
+        zipname = zip_name_area.get()
+    shutil.make_archive(zipname, 'zip', pathname)
 
-# Sets up the labels & selection boxes
-boxLabel = Label(app, text="Path Of The Folder Selected")
-boxLabel.grid(row=1,column=2, padx=40)
-pathlabel = Label(app)
-pathlabel.grid(row=2,column=2, padx=40)
+app = Tk()
 
-boxLabel2 = Label(app, text="Path Zip Has Been Saved To")
-boxLabel2.grid(row=3,column=2, padx=40)
-currentPath = Label(app)
-currentPath.grid(row=4,column=2, padx=40)
+browsebutton = Button(app, text="Browse", command=browse, fg="#708090", bg="white" ,font=("Helvetica", 14, "bold"))
+browsebutton.grid(row=1,column=1, pady=20, padx=10, sticky=W)
 
-# Sets up the application title, background colour and window size
+# Path of the selected File Name
+boxLabel = Label(app, text="Path Of The Folder Selected", fg="white", bg="#708090" ,font=("Helvetica", 12, "bold"))
+boxLabel.grid(row=1,column=2, padx=10, sticky=W)
+
+# Displays the path of the selected file
+pathlabel = Label(app, fg="white", bg="#708090" ,font=("Helvetica", 12, "bold"))
+pathlabel.grid(row=1,column=2, padx=5, sticky=W)
+
+# Zipname Field 
+zip_label = Label(app,text="Enter Zip Name", fg="white", bg="#708090" ,font=("Helvetica", 12, "bold"))
+zip_label.grid(row=2, column=1, padx=10, pady=20, sticky=W, columnspan=2)
+
+# Zipname Label
+zip_name_area = Entry(app,font=14)
+zip_name_area.grid(row=2, column=2, padx=10, pady=20, sticky=W)
+
+# Zip Button
+zipButton = Button(app, text="Zip Folder", command=zipFolder, fg="#708090", bg="white" ,font=("Helvetica", 14, "bold"))
+zipButton.grid(row=3,column=1, pady=20, padx=10, sticky=W)
+
+# Zip Path Label
+boxLabel2 = Label(app, text="Path Zip Has Been Saved To", fg="white", bg="#708090" ,font=("Helvetica", 12, "bold"))
+boxLabel2.grid(row=3,column=2, padx=10, sticky=W, columnspan=2)
+
+# Zip Path
+currentPath = Label(app, text="Path Zip Has Been Saved To", fg="white", bg="#708090" ,font=("Helvetica", 12, "bold"))
+currentPath.grid(row=3,column=2, padx=10, sticky=W)
+
+# Set zip_now_button state disabled for default
+zipButton["state"] = "disabled"
+
 app.title("Zip FIle")
-app.geometry("250x180")
 app.configure(bg='#708090')
 
 app.mainloop()
