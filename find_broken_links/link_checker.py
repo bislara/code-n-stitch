e73eb18086d32938f1f11e196601871eae424073
cyrilico <jpdamas15@gmail.com>
2020-10-09 16:14:49 +0100

    Fix some pylint warnings

--- find_broken_links/link_checker.py
+++ find_broken_links/link_checker.py
@@ -1,7 +1,7 @@
-import bs4, requests, html
+from urllib.parse import urlsplit
 import re
-import urllib.request
-from urllib.parse import urlsplit, urlunsplit
+import bs4
+import requests
 
 def link_broken(link):
     """
@@ -12,11 +12,7 @@ def link_broken(link):
         #Raise for status creates HTTPError exception if request is unsuccessful
         res.raise_for_status()
         return False
-    except requests.HTTPError as e:
-        print ("HTTP Error {}".format(e))
-        return True
-    except Exception as e:
-        print ("Error {}".format(e))
+    except requests.HTTPError:
         return True
 
 def find_broken_links(address):
@@ -28,15 +24,13 @@ def find_broken_links(address):
 
     #Create soup to get page HTML
     res = requests.get(address)
-    res.raise_for_status()
-
     http_encoding = res.encoding if 'charset' in res.headers.get('content-type', '').lower() else None
     html_encoding = bs4.dammit.EncodingDetector.find_declared_encoding(res.content, is_html=True)
     encoding = html_encoding or http_encoding
     soup = bs4.BeautifulSoup(res.content,"html.parser", from_encoding=encoding)
 
     #Iterate over visible elements with potential links
-    for element, attribute in iter(elem_dict.items()):   
+    for element, attribute in iter(elem_dict.items()):
         try:
             for link_elem in soup.select(element):
                 if link_elem.has_attr(attribute):
@@ -53,20 +47,14 @@ def find_broken_links(address):
                         url_to_test = address + link_elem[attribute]
                     #Special case of no protocol, non-relative URL
                     elif address_split.scheme == '' and (address_split.netloc != '' or address_split.path != ''):
-                        address_unsplit = urlunsplit(address_split)
-                        if re.match('^//' , address_unsplit):
-                            m = re.search(r'(?<=//)\S+', address_unsplit)
-                            address_unsplit = m.group(0)  
-                            url_to_test = "https://" + address_unsplit
-                    
+                        if re.match('^//' , url_to_test):
+                            path_match = re.search(r'(?<=//)\S+', url_to_test)
+                            url_to_test = path_match.group(0)
+                            url_to_test = "https://" + url_to_test
+
                     #Finally test URL to check if it is broken
                     if link_broken(url_to_test):
                         links.append(url_to_test)
-        except requests.HTTPError as e:
-            print(e)
-        except Exception as e:
-            print(e)
+        except requests.HTTPError as http_excpt:
+            print(http_excpt)
     return links
-
-if __name__ == '__main__':
-    print(find_broken_links('https://cyrili.co/'))
