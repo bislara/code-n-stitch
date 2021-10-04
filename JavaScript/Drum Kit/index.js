dd5921794413ef556c4930a8483925e27efd6d2c
kavya_kunder <kavyakunder7082@gmail.com>
2021-10-04 19:03:21 +0530

    add drum kit project by kavya kunder

---
+++ JavaScript/Drum Kit/index.js
@@ -0,0 +1,102 @@
+document.addEventListener("keydown",function(even){
+  makeSound(even.key);
+  keyPressed(even.key);
+
+});
+
+
+// document.addEventListener("keydown",function()
+// {
+//   alert("keyy");
+// });
+for( var i=0;i<=(document.querySelectorAll(".drum").length);i++){
+document.querySelectorAll("button")[i].addEventListener("click",function()
+{
+// var changeColor=this.style.color="white";
+//   // console.log(changeColor);
+//   var audio=new Audio("sounds/kick-bass.mp3");
+//   audio.play();
+
+makeSound(this.innerHTML);
+keyPressed(this.innerHTML);
+}
+);
+};
+
+
+
+
+
+//
+//
+// // function keyIs()
+// // {
+// //   alert("hello");
+// // }
+
+function makeSound(key)
+{
+  switch (key) {
+    case "w":
+
+    var audio1=new Audio("sounds/tom-1.mp3");
+     audio1.play();
+      break;
+      case "a":
+
+      var audio2=new Audio("sounds/tom-2.mp3");
+       audio2.play();
+        break;
+        case "s":
+
+        var audio3=new Audio("sounds/tom-3.mp3");
+         audio3.play();
+          break;
+          case "d":
+
+          var audio4=new Audio("sounds/tom-4.mp3");
+           audio4.play();
+            break;
+            case "j":
+
+            var audio5=new Audio("sounds/crash.mp3");
+             audio5.play();
+              break;
+              case "k":
+
+              var audio6=new Audio("sounds/kick-bass.mp3");
+               audio6.play();
+                break;
+                case "l":
+
+                var audio7=new Audio("sounds/snare.mp3");
+                 audio7.play();
+                  break;
+
+    default:
+
+  }
+  //
+}
+function keyPressed(currentKey)
+{
+
+  var animate=document.querySelector("."+currentKey);
+  animate.classList.add("pressed");
+
+
+
+
+  setTimeout(function()
+  { animate.classList.remove("pressed"); },
+   500);
+
+
+
+
+
+
+
+
+
+}
