a14255ff2becadb6e17c994f4e642b3400888fb8
Dishant <keenlearner13579@gmail.com>
2021-10-24 01:55:20 +0530

    Added user-input and exception-handling

--- Python/speech-to-text/speech-to-text.py
+++ Python/speech-to-text/speech-to-text.py
@@ -1,5 +1,5 @@
 import speech_recognition as sr
-filename = "sample.wav"
+filename = input("Enter filename (without extenstion): ")+".wav"
 
 # Initializing speech recognizer
 r = sr.Recognizer()
@@ -8,8 +8,14 @@ r = sr.Recognizer()
 with sr.AudioFile(filename) as source:
     # Loading audio data onto memory
     audio_data = r.record(source)
-    print('Audio data successfully loaded onto memory.\nConverting speech to text...')
+    print('Audio data successfully loaded from ' + filename + ' to memory.\nConverting speech to text...')
     # Recognizing speech and converting to text.
-    text = r.recognize_google(audio_data)
-    print('Speech-to-Text conversion done.\n-------------------------------------------\nOutput:')
-    print(text+'\n')
+    
+    try:
+        text = r.recognize_google(audio_data)
+        print('Speech-to-Text conversion done.\n------------------------------------------------\nOutput:')
+        print(text+'\n')
+    except:
+        print('Couldn\'t establish connection with Google server.')
+        print('---------------------------------------------------\nSpeech-to-Text conversion failed. Please try again.')
+    
